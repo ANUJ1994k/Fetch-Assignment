f99ba900fa247d992c33eb133d075018bd0e5e3a
anuj1994k <anuj1994k@gmail.com>
2024-06-21 08:56:31 +0530

    final code for pushing and submitting

--- Assignment1/index.js
+++ Assignment1/index.js
@@ -9,7 +9,6 @@ async function getCategoriesData() {
         const response = await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood');
         const data = await response.json();
         console.log(data);
-        displayData(data);
     } catch (error) {
         console.error('something went wrong:', error);
     }
@@ -28,46 +27,6 @@ async function getIngredientData() {
 }
 
 // Event listeners for buttons
-btn1.addEventListener('click', getCategoriesData,displayData);
-btn2.addEventListener('click', getIngredientData ,displayData);
+btn1.addEventListener('click', getCategoriesData);
+btn2.addEventListener('click', getIngredientData);
 
-// Function to create a card element
-function createCard(info) {
-    let card = document.createElement('div');
-    card.className = 'card';
-    
-    let img = document.createElement('img');
-    img.src = meal.strMealThumb;
-    img.alt = meal.strMeal;
-
-    let title = document.createElement('h3');
-    title.textContent = meal.strMeal;
-
-    card.append(title,img)
-
-    return card;
-}
-
-// Function to display data in cards
-
-function displayData(data) {
-    data.forEach( (user)=> {
-      let div = document.createElement("div");
-
-      let avatar = document.createElement("img");
-
-      avatar.src = user.avatar;
-
-      let name = document.createElement("p");
-
-      name.innerText = `${user.first_name} ${user.last_name}`;
-
-      let email = document.createElement("p");
-
-      email.innerText = user.email;
-
-      div.append(avatar, name, email);
-
-      container.appendChild(div);
-    });
-  }
